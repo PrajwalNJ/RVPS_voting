1aaa512256a3588564ce86aeba9de1ee99448abb
PrajwalNJ <38159883+PrajwalNJ@users.noreply.github.com>
2018-04-23 15:47:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+$("#sidebar_button").click(function(e){
+    e.preventDefault();
+    $("#wrapper").toggleClass("active");
+    $("#sidebar_button a").toggleClass("active");
+});
+$("#top_menu li").click(function (e) {
+    e.preventDefault();
+    $("#top_menu li").toggleClass("active");
+});
+
+function votingDisplay(){
+    var error="";
+    if(isNaN(document.getElementById('roll-number').value))
+        error+="Invalid Entry for Roll Number !! \n";
+    if(document.getElementById('class').options[document.getElementById('section').selectedIndex].text=="Select Class")
+        error+="Select your class ! \n";
+    if(document.getElementById('section').options[document.getElementById('section').selectedIndex].text=="Select Section")
+        error+="Select your section ! \n";
+    if(document.getElementById('house').options[document.getElementById('house').selectedIndex].text=="Select House")
+        error+="Select your house ! \n";
+    if(!(error==""))
+        alert(error);
+    else{
+        document.getElementById('cast-vote_default').style.display='block';
+        document.getElementById('school').style.display='-webkit-box';
+        for(var i=1;i<5;i++)
+            document.getElementsByClassName('election-type')[i].style.display='none';
+        document.getElementById(document.getElementById('house').value.toLowerCase()).style.display='-webkit-box';
+        $('#voting-modal').modal('hide');
+    }
+}
+
+function checkRadioButton(name,index) {
+    document.getElementsByName(name)[index].checked=true;
+}
